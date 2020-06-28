59ac1bc65243816ee352de3d885979898489ed64
Yash Paneliya <yashsp1158@gmail.com>
2020-06-28 10:00:00 +0530

    Initial commit

---
+++ android/app/src/main/kotlin/com/example/windows10_1990/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.windows10_1990
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/main.dart
@@ -0,0 +1,11 @@
+import 'package:flutter/material.dart';
+import 'package:windows10_1990/screens/bootingUp.dart';
+
+void main(){
+  runApp(MaterialApp(
+    debugShowCheckedModeBanner: false,
+    title: 'Windows 10 1990 Edition',
+    home: SplashScreen(),
+  ));
+}
+
