cc5166bb17ff72c0a86a488c98246401f7bc57b2
Yash Paneliya <yashsp1158@gmail.com>
2020-06-28 15:43:15 +0530

    Color pallete options added

--- lib/res/colors.dart
+++ lib/res/colors.dart
@@ -9,3 +9,25 @@ Color darkBlue = Colors.blue.shade900;
 Color normalgrey = Colors.grey;
 Color bordergrey = Colors.grey.shade400;
 Color greyBem = Colors.black.withOpacity(0.5);
+
+List<Color> coloroptions = [
+  Colors.white,
+  Color.fromRGBO(191, 189, 189, 1),
+  Colors.blue.shade800,
+  Colors.yellow,
+  Colors.green,
+  Colors.red,
+  Colors.purple,
+  Colors.cyan,
+  Colors.amber,
+  Colors.orange,
+  Colors.black,
+  Colors.deepOrange,
+  Colors.pink,
+  Colors.amberAccent,
+  Colors.deepPurpleAccent,
+  Colors.limeAccent,
+  Colors.pinkAccent,
+  Colors.lime,
+  Colors.blueAccent
+];
