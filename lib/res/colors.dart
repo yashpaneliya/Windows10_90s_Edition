18fb5ae8b22406a72933279feb9c7daf4b4f4ea7
Yash Paneliya <yashsp1158@gmail.com>
2020-06-28 09:54:42 +0530

    Resources

---
+++ lib/res/colors.dart
@@ -0,0 +1,11 @@
+import 'package:flutter/material.dart';
+
+Color bottomBarColor = Colors.black;
+Color grey = Colors.grey.shade900;
+Color white = Colors.white;
+Color startMenuBack = Color.fromRGBO(191, 189, 189, 1);
+Color blue = Colors.blue.shade800;
+Color darkBlue = Colors.blue.shade900;
+Color normalgrey = Colors.grey;
+Color bordergrey = Colors.grey.shade400;
+Color greyBem = Colors.black.withOpacity(0.5);
