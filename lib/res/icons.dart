ccfdceef4b834ffc267c91f59740929f62358915
Yash Paneliya <yashsp1158@gmail.com>
2020-06-28 15:43:47 +0530

    toolbar icons added

---
+++ lib/res/icons.dart
@@ -0,0 +1,18 @@
+import 'package:flutter/material.dart';
+
+List<IconData> toolbarIcons = [
+  Icons.edit,
+  Icons.remove,
+  Icons.search,
+  Icons.sort_by_alpha,
+  Icons.line_style,
+  Icons.polymer,
+  Icons.add_circle,
+  Icons.crop_square,
+  Icons.star,
+  Icons.color_lens,
+  Icons.redo,
+  Icons.undo,
+  Icons.brush,
+  Icons.border_all
+];
